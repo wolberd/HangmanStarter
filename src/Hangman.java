611152338bd2b98e739c791c047a4e17f3fa1d76
David Wolber <wolber@gmail.com>
2019-08-18 12:07:25 -0700

    initial hangman starter code

---
+++ src/Hangman.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Random;
+
+
+public class Hangman {
+
+	public static void main(String [] args) {
+
+        Hangman hangman = new Hangman();
+
+        List<String> phraseList=null;
+        // Get the phrase from a file of phrases
+        try {
+            phraseList = Files.readAllLines(Paths.get("phrases.txt"));
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        System.out.println(phraseList);
+    }
+
+}
