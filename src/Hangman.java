f3a8a4332b9b5d3a18f89d0fb7bdef17bded9f0a
David Wolber <wolberd@usfca.edu>
2020-01-20 10:14:55 -0800

    Update Hangman.java

--- src/Hangman.java
+++ src/Hangman.java
@@ -9,16 +9,16 @@ public class Hangman {
 
 	public static void main(String [] args) {
 
-        Hangman hangman = new Hangman();
+        	Hangman hangman = new Hangman();
 
-        List<String> phraseList=null;
-        // Get the phrase from a file of phrases
-        try {
-            phraseList = Files.readAllLines(Paths.get("phrases.txt"));
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-        System.out.println(phraseList);
-    }
+        	List<String> phraseList=null;
+        	// Get the phrase from a file of phrases
+        	try {
+            		phraseList = Files.readAllLines(Paths.get("phrases.txt"));
+        	} catch (IOException e) {
+        	    	System.out.println(e);
+        	}
+        	System.out.println(phraseList);
+    	}
 
 }
